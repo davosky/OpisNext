bc30f41d6cb4f88c458f2e88e2465b70820acf83
Davo Davosky <davodavosky@gmail.com>
2021-12-19 19:01:50 +0100

    Fixed Migrations

--- db/migrate/20211101102236_create_categories.rb
+++ db/migrate/20211101102236_create_categories.rb
@@ -1,6 +1,6 @@
 class CreateCategories < ActiveRecord::Migration[6.1]
   def change
-    create_table :inca_offices do |t|
+    create_table :categories do |t|
       t.string :name
       t.integer :position
 
